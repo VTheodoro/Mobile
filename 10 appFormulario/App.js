fcea9236fa1a83ba2278f6022f0e8fdc5f8d4897
2º ETIM - Desenvolvimento de Sistemas <ds-etim-2@servidor.acad>
2024-04-18 13:11:23 -0300

    Update!

--- 10 appFormulario/App.js
+++ 10 appFormulario/App.js
@@ -13,7 +13,7 @@ export default function App() {
   const [placa, setPlaca] = useState('');
   const [modelo, setMoledo] = useState('0');
   const [valor, setValor] = useState('15000');
-  const [utilitario, setulitario] = useState(false);
+  const [utilitario, setUlitario] = useState(false);
   const [checked, setChecked] = useState('Masculino');
 
   const modelos = [
@@ -37,16 +37,74 @@ export default function App() {
         <View style={styles.areaRadio}>
           <RadioButton
             value="Masculino"
-            status={chacked == 'Masculino' ? 'checked' : 'unchecked'}
+            status={checked == 'Masculino' ? 'checked' : 'unchecked'}
             onPress={() => setChecked('Masculino')}
           >
-
           </RadioButton>
 
+          <RadioButton
+            value="Feminino"
+            status={checked == 'Feminino' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('Feminino')}
+          >
+          </RadioButton>
+        </View>
+
+        <Text style={styles.textoNome}>Carro:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite o nome do Carro:'
+          onChangeText={carro => setCarro(carro)}
+        >
+        </TextInput>
+
+        <Text style={styles.textoNome}>Placa:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Digite a Placa do carro:'
+          onChangeText={placa => setPlaca(placa)}
+        >
+        </TextInput>
+
+        <View style={styles.areaModelo}>
+
+          <Text style={styles.textoNome}>Modelo:</Text>
+          <Picker
+            style={styles.pickerModelo}
+            selectedValue={modelo}
+            onValueChange={(itemValue, itemIndex) => setMoledo(itemValue)}
+          >
+            {/*Lembrar de colocar item*/}
+          </Picker>
+        </View>
+
+        <View style={styles.valorCarro}>
+          <Text style={styles.textoNome}>Valor do Carro:</Text>
+          <Text style={styles.carroTexto}>R${valor.toFixed(0)}</Text>
+        </View>
+
+        <View style={styles.areaSlider}>
+          <Slider
+            minimumTrackTintColor='#CF0'
+            minimumValue={15000}
+            maximumValue={300000}
+            value={valor}
+            onValueChange={(valor) => setValor(valor)}
+          >
+          </Slider>
+        </View>
+
+        <View style={styles.areaUtilitario}>
+          <Text style={styles.textoNome}>Utilitário:</Text>
+          <Switch
+            style={{ paddingTop: 15 }}
+            value={utilitario}
+            onValueChange={(utilitario) => stUtilitario(utilitario)}
+          >
+          </Switch>
         </View>
 
       </View>
-
     </View>
   );
 }
